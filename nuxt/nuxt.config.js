43b5cfa9d327199aaa504da56d2cde2aa0e21080
Stuart Clark <stu@rtclark.net>
2021-07-31 07:42:35 +0000

    [#1] feat: add netlify deployment.

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -4,6 +4,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  generate: {
+    routes: ['/'],
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'example-blog-nuxt',
