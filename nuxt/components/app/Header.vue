beb271a69bb9551f95d805f0ddab938ba6c7b3ca
Stuart Clark <stuart@realityloop.com>
2021-08-01 11:49:04 +1000

    [#7] feat: add default layout and header.

---
+++ nuxt/components/app/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div
+    class="navbar mb-2 shadow-lg bg-neutral text-neutral-content rounded-box"
+  >
+    <div class="flex-1 px-2 mx-2">
+      <NuxtLink class="text-lg font-bold" to="/">DruxtBlog</NuxtLink>
+    </div>
+    <div class="flex-none">
+      <slot />
+    </div>
+  </div>
+</template>
---
+++ nuxt/layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="container mx-auto py-10">
+    <AppHeader class="mb-10" />
+    <Nuxt />
+  </div>
+</template>
