3ef6b59b79628670d9decc0a7e5419fce9953aa7
Stuart Clark <stuart@realityloop.com>
2021-08-01 21:02:41 +1000

    [#7] feat: add layout and drawer.

---
+++ nuxt/components/app/Drawer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="drawer drawer-end min-h-screen">
+    <input id="my-drawer" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content" :class="$attrs['content-class']">
+      <slot />
+    </div>
+
+    <div class="drawer-side h-100">
+      <label for="my-drawer" class="drawer-overlay"></label>
+      <div class="p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+        <slot name="drawer" />
+      </div>
+    </div>
+  </div>
+</template>
--- nuxt/layouts/default.vue
+++ nuxt/layouts/default.vue
@@ -1,6 +1,92 @@
 <template>
-  <div class="container mx-auto py-10">
-    <AppHeader class="mb-10" />
-    <Nuxt />
-  </div>
+  <component :is="component.is" v-bind="component">
+    <AppHeader class="flex-none mb-10">
+      <label
+        v-if="isDev"
+        for="my-drawer"
+        class="btn btn-square btn-ghost drawer-button"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm7.707 3.293a1 1 0 010 1.414L9.414 9H17a1 1 0 110 2H9.414l1.293 1.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </label>
+    </AppHeader>
+
+    <main class="flex-grow">
+      <Nuxt />
+    </main>
+
+    <div>
+      <a href="https://druxtjs.org" class="link-secondary" target="_blank"
+        >DruxtJS.org</a
+      >
+    </div>
+
+    <template v-if="isDev" #drawer>
+      <label class="label">
+        <span class="label-text">Theme</span>
+      </label>
+      <select class="select select-bordered w-full max-w-xs" v-model="theme">
+        <option disabled="disabled" selected="selected">Select a theme</option>
+        <option v-for="theme of themes" :key="theme">{{ theme }}</option>
+      </select>
+    </template>
+  </component>
 </template>
+
+<script>
+export default {
+  head: ({ theme }) => ({
+    htmlAttrs: {
+      'data-theme': theme,
+    },
+  }),
+
+  data: () => ({
+    classes: 'container flex flex-col mx-auto py-10',
+    theme: 'light',
+    themes: [
+      'light',
+      'dark',
+      'cupcake',
+      'bumblebee',
+      'emerald',
+      'corporate',
+      'synthwave',
+      'retro',
+      'cyberpunk',
+      'valentine',
+      'halloween',
+      'garden',
+      'forest',
+      'aqua',
+      'lofi',
+      'pastel',
+      'fantasy',
+      'wireframe',
+      'black',
+      'luxury',
+      'dracula',
+    ],
+  }),
+
+  computed: {
+    component: ({ isDev }) => ({
+      [isDev ? 'content-class' : 'class']:
+        'container flex flex-col mx-auto py-10',
+      is: isDev ? 'AppDrawer' : 'div',
+    }),
+
+    isDev: ({ $nuxt }) => $nuxt.context.isDev,
+  },
+}
+</script>
